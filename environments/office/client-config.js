5e56c142f0a4dec776008992a8eec8e686936e5f
sinny777 <sinny777@gmail.com>
2018-03-26 09:29:50 +0530

    A few Changes

--- environments/office/client-config.js
+++ environments/office/client-config.js
@@ -7,30 +7,7 @@
 
 define(['angular'], function (angular) {
 	'use strict';
-  
-	return angular.module('app.config', [])
-		.constant('CONFIG', {
-			VERSION: '0.1',
-			ENVIRONMENT: 'LOCAL',
-			API_URL: '//hukam-dev.mybluemix.net/api',
-			IOT_CONFIG:{
-				"org": "rqeofj",
-			    "id": "a-rqeofj-i7nm1fdo5d",
-			    "authkey": "a-rqeofj-i7nm1fdo5d",
-			    "authtoken": "3X0bTD*8tf4Q1rq12n",
-			    "type": "shared",
-			    "gatewayType": "HukamGateway"
-			}
-		});
-    
-});
 
-// API_URL: 'http://granslive-web.mybluemix.net/api'
-// API_URL: 'http://localhost:3000/api'
-
-define(['angular'], function (angular) {
-	'use strict';
-  
 	return angular.module('app.config', [])
 		.constant('CONFIG', {
 			VERSION: '0.1',
@@ -45,5 +22,5 @@ define(['angular'], function (angular) {
 			    "gatewayType": "HukamGateway"
 			}
 		});
-    
+
 });
